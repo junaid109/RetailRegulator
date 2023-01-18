b1a84f0c472d522cf4ed6a01ed1ea7c30bcf2644
Junaid Malik <junmalik109@gmail.com>
2023-01-18 12:17:59 +0000

    Initial project for MVC app created

---
+++ RetailRegulator/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetailRegulator.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id
+		{
+			get; set;
+
+		}
+		public string CategoryName { get; set; }
+
+		public string Description { get; set; }
+
+		public int DisplayOrder { get; set; }
+
+		public byte[] Picture { get; set; }
+
+		public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+		public DateTime ModifiedDate { get; set; }
+	}
+}
