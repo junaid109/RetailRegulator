786ab50ee1461b64baa9f590e5a7ba791282dc9a
Junaid Malik <junmalik109@gmail.com>
2023-02-08 21:08:25 +0000

    Create ApplicationDbContext.cs

---
+++ RetailRegulator/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailRegulator.Models;
+
+namespace RetailRegulator.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+		
+		public DbSet<Category> Categories { get; set; }
+		
+	}
+}
